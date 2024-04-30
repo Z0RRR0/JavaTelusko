70d2338ce83d1275e17588eb176d31da66ef9bef
Tushar Bhatt <128506340+Z0RRR0@users.noreply.github.com>
2024-04-30 10:29:53 +0530

    Add files via upload

---
+++ corejava/datatype.java
@@ -0,0 +1,19 @@
+class datatype
+{
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c ='k';
+        char c1 = '8';        //literals
+
+        boolean b =true;
+        System.out.println(b);
+    }
+}
---
+++ corejava/main.java
@@ -0,0 +1,15 @@
+class main
+{
+    public static void main(String a[])
+    {
+        //literals
+
+            int num1 = 0x7E;
+            int num2 = 10_00_00_000;
+            char c= 'a';
+            c++;
+            System.out.println(c);
+            System.out.println(num1);
+            System.out.println(num2);
+    }
+}
